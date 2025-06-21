ab13087eb212decbc7b47912899476ecca08fa5d
Daa Lee <olcror8@gmail.com>
2025-06-21 22:06:00 +0900

    Create 03_input_output.py

---
+++ 03_input_output.py
@@ -0,0 +1,11 @@
+# -----------------------------------------
+# 📌 User Input and Output
+# -----------------------------------------
+
+name = input("What is your name? ")
+message = "hi, " +name+ ". nice to meet you ... bye, " +name+ "."
+print(message)
+# == Output ===============================
+# What is your name? Daa
+# hi, Daa. nice to meet you ... bye, Daa.
+# =========================================
