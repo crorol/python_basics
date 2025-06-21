a6977e139abe7eb66fa4fe0bbb96798662684cb5
Daa Lee <olcror8@gmail.com>
2025-06-22 00:07:19 +0900

    Create 09_tuples_dicts_sets.py

---
+++ 09_tuples_dicts_sets.py
@@ -0,0 +1,11 @@
+# tuples
+point = (3, 4)
+print(point[0])  # Output: 3
+
+# dictionaries
+person = {"name": "Daa", "age": 21}
+print(person["name"])  # Output: Daa
+
+# sets
+numbers = {1, 2, 3, 1}
+print(numbers)  # Output: {1, 2, 3}
