7221eea23fd42b5888c10fdb6b95e61f2373e967
Daa Lee <olcror8@gmail.com>
2025-06-21 22:08:10 +0900

    Create 04_numbers.py

---
+++ 04_numbers.py
@@ -0,0 +1,30 @@
+# -----------------------------------------
+# 📌 Data Type and Number
+# -----------------------------------------
+
+# integer: 0, 11, -19
+# float: 0.0, -20.24
+
+# exponentiation in Python
+print(0.11924e2)   # Output: 11.924
+print(0.11924e-2)  # Output: 0.0011924
+
+# +, -, *, /, //, %, **
+print("1 + 2 =", 1 + 2)    # Output: 1 + 2 = 3
+print("4 - 2 =", 4 - 2)    # Output: 4 - 2 = 2
+print("2 * 3 =", 2 * 3)    # Output: 2 * 3 = 6
+print("8 / 2 =", 8 / 2)    # Output: 8 / 2 = 4.0
+print("7 // 3 =", 7 // 3)  # Output: 7 // 3 = 2
+print("5 % 2 =", 5 % 2)    # Output: 5 % 2 = 1
+print("2 ** 3 =", 2 ** 3)  # Output: 2 ** 3 = 8
+
+
+# math module: sqrt() & pow()
+import math
+print('math.sqrt(81)', math.sqrt(81))     # Output: math.sqrt(81) 9.0
+print('math.pow(11,2)', math.pow(11,2))   # Output: math.pow(11,2) 121.0
+
+
+# random module
+import random
+print('random.random()', random.random())  # Output: random.random() 0.36782793059171537
