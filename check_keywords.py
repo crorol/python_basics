bc3665140764f05094c9e7f76cd14142ac97010a
Daa Lee <olcror8@gmail.com>
2025-06-21 21:04:36 +0900

    Update check_keywords.py

--- check_keywords.py
+++ check_keywords.py
@@ -1,6 +1,7 @@
 # -----------------------------------------
 # 📌 How to check Python keywords
 # -----------------------------------------
+
 import keyword
 print(keyword.kwlist)  # Output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 print(len(keyword.kwlist))  # Output: 35
