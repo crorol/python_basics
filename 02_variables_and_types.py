460ec7efef31e0ae3308268d29f7fb1e10dd08ac
Daa Lee <olcror8@gmail.com>
2025-06-21 21:57:05 +0900

    Create 02_variables_and_types.py

---
+++ 02_variables_and_types.py
@@ -0,0 +1,13 @@
+# -----------------------------------------
+# 📌 Variables and Types
+# -----------------------------------------
+
+name = "Daa Lee"
+age = 21
+height = 159.7
+is_student = True
+
+print(type(name))        # Output: <class 'str'>
+print(type(age))         # Output: <class 'int'>
+print(type(height))      # Output: <class 'float'>
+print(type(is_student))  # Output: <class 'bool'>
