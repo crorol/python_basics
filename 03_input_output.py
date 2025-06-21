0a86505685488b5025fd02e2f8a435b0d5e8247b
Daa Lee <olcror8@gmail.com>
2025-06-21 22:06:23 +0900

    Update 03_input_output.py

--- 03_input_output.py
+++ 03_input_output.py
@@ -5,6 +5,7 @@
 name = input("What is your name? ")
 message = "hi, " +name+ ". nice to meet you ... bye, " +name+ "."
 print(message)
+
 # == Output ===============================
 # What is your name? Daa
 # hi, Daa. nice to meet you ... bye, Daa.
